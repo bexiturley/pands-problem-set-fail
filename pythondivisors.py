6f2d7437e9fced50e804ce85bcc136d6338908b5
bexiturley <47194968+bexiturley@users.noreply.github.com>
2019-02-13 22:03:42 +0000

    Update pythondivisors.py
    Add not 12

--- pythondivisors.py
+++ pythondivisors.py
@@ -1,8 +1,12 @@
+# Solution to problem 3
 # Rebecca Turley, 2019-02-13
 # python divisors.py
 
-nl=[]
-for x in range(1000, 10000):
-    if (x%6==0)  and (x%12==0):
-        nl.append(str(x))
-print (','.join(nl))
+nl = []
+for x in range (1000, 10000) :
+
+    if (x % 6 == 0)  and (x % 12 != 0) :
+
+        nl.append (str (x) )
+
+print (','.join (nl) )
